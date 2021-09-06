c7345af9e01b3dbc48ae28e8afe8b8fc9ab22e9c
ez-queze <85289217+ez-queze@users.noreply.github.com>
2021-09-06 13:35:10 +0800

    upload project

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('img/logo.png') }}" width="250vw" alt="logo">
---
+++ resources/views/visi.blade.php
@@ -0,0 +1,36 @@
+<x-app-layout>
+<x-slot name="title">
+        Visi & Misi
+    </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Visi & Misi') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                <br>
+                <h2 class="text-center">Visi</h2>
+                <br>
+                <h4  class=" text-center fst-italic ">“Terwujudnya Sikap dan Prilaku Kewirahusahaan Melalui Penerapan Pola Manajemen Partisipatif yang Berpotensi Pada Pandayaguna <br> Teknologi Informasi yang Berakses Global“</h4>
+                <br>
+               <div class="border-bottom border border-light"></div>
+
+                <br>
+                <h2 class="container text-center">Misi </h2>
+                <br>
+                    <p class="col-md-10 ms-auto fs-6 " >
+                        1.  Menumbuhkembangkan sikap kewirahusaan setiap tamatan sebagai cerminan penguasaan kompetensi untuk bekal hidup secara mandiri. <br>
+                        2.  Meningkatkan kompetensi pendidikan dan tenaga kependidikan, serta seluruh peserta didik baik secara internal maupun eksternal. <br>
+                        3.  Menumbuhkan penggunaan teknologi informasi yang berakses global.<br>
+                    </p>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
